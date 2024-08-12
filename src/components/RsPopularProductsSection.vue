4ef90446ab56700a40c5158bdcb1892d9a98dc48
Hardart <kaino@bk.ru>
2024-08-12 14:49:34 +0300

    feat: add various new components to homepage layout

---
+++ src/components/RsPopularProductsSection.vue
@@ -0,0 +1,135 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="popular-products-section">
+    <h3 class="section-title">DC Shoes популярное в коллекции</h3>
+    <div class="products-carousel">
+      <div class="products-carousel__items">
+        <article class="product-card">
+          <div class="product-card__header">
+            <img
+              class="product-card__image"
+              src="../assets/images/image 9-3.png"
+              alt=""
+            />
+            <div class="product-card__favorite">
+              <svg class="rs-icon">
+                <use xlink:href="@/assets/icons/sprite.svg#star"></use>
+              </svg>
+            </div>
+            <div class="product-card__info">
+              <p class="product-card__badge product-card__badge--accent">
+                -50%
+              </p>
+            </div>
+          </div>
+          <div class="product-card__body">
+            <h4 class="product-card__title">lib tech</h4>
+            <p class="product-card__description">Мужской Сноуборд</p>
+            <div class="product-card__pricing">
+              <span class="product-card__price product-card__price--old">
+                34392 ₽
+              </span>
+              <span class="product-card__price product-card__price--new">
+                17392 ₽
+              </span>
+            </div>
+          </div>
+        </article>
+        <article class="product-card">
+          <div class="product-card__header">
+            <img
+              class="product-card__image"
+              src="../assets/images/image 9-4.png"
+              alt=""
+            />
+            <div class="product-card__favorite">
+              <svg class="rs-icon">
+                <use xlink:href="@/assets/icons/sprite.svg#star"></use>
+              </svg>
+            </div>
+            <div class="product-card__info">
+              <p class="product-card__badge product-card__badge--accent">
+                -50%
+              </p>
+            </div>
+          </div>
+          <div class="product-card__body">
+            <h4 class="product-card__title">lib tech</h4>
+            <p class="product-card__description">Мужской Сноуборд</p>
+            <div class="product-card__pricing">
+              <span class="product-card__price product-card__price--old">
+                34392 ₽
+              </span>
+              <span class="product-card__price product-card__price--new">
+                17392 ₽
+              </span>
+            </div>
+          </div>
+        </article>
+        <article class="product-card">
+          <div class="product-card__header">
+            <img
+              class="product-card__image"
+              src="../assets/images/image 9-1.png"
+              alt=""
+            />
+            <div class="product-card__favorite">
+              <svg class="rs-icon">
+                <use xlink:href="@/assets/icons/sprite.svg#star"></use>
+              </svg>
+            </div>
+          </div>
+          <div class="product-card__body">
+            <h4 class="product-card__title">lib tech</h4>
+            <p class="product-card__description">Мужской Сноуборд</p>
+            <div class="product-card__pricing">
+              <span class="product-card__price"> 34392 ₽ </span>
+            </div>
+          </div>
+        </article>
+        <article class="product-card">
+          <div class="product-card__header">
+            <img
+              class="product-card__image"
+              src="../assets/images/image 9-2.png"
+              alt=""
+            />
+            <div class="product-card__favorite">
+              <svg class="rs-icon">
+                <use xlink:href="@/assets/icons/sprite.svg#star"></use>
+              </svg>
+            </div>
+          </div>
+          <div class="product-card__body">
+            <h4 class="product-card__title">lib tech</h4>
+            <p class="product-card__description">Мужской Сноуборд</p>
+            <div class="product-card__pricing">
+              <span class="product-card__price"> 34392 ₽ </span>
+            </div>
+          </div>
+        </article>
+      </div>
+      <div class="carousel-navigation carousel-navigation--left">
+        <svg class="rs-icon carousel-navigation__icon">
+          <use xlink:href="@/assets/icons/sprite.svg#chevron-left"></use>
+        </svg>
+      </div>
+      <div class="carousel-navigation carousel-navigation--right">
+        <svg class="rs-icon carousel-navigation__icon">
+          <use xlink:href="@/assets/icons/sprite.svg#chevron-left"></use>
+        </svg>
+      </div>
+    </div>
+    <div class="section-show-more-button">
+      <button class="rs-button rs-button--secondary">Показать больше</button>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.popular-products-section {
+  margin: 20px 0;
+  padding: 40px 0;
+}
+</style>
---
+++ src/components/RsSpecialOffer.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="special-offer">
+    <img class="special-offer__image" src="../assets/images/offer.png" />
+    <h4 class="special-offer__title">название акции</h4>
+    <p class="special-offer__description">
+      Меня не спрашивали, а надо было спросить, что означает имя Заратустры
+      именно в моих устах — в устах первого имморалиста: ведь то, в чём состоит
+      неслыханная уникальность этого перса в истории, являет собою
+      противоположность как раз этому.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.special-offer {
+  position: relative;
+  margin: 35px 0;
+
+  &__image {
+    width: 100%;
+    max-height: 350px;
+    object-fit: cover;
+  }
+
+  &__title {
+    margin-top: 25px;
+    font-size: var(--fs-2xl);
+    font-weight: var(--fw-medium);
+    line-height: 35px;
+    text-transform: capitalize;
+    text-align: center;
+  }
+
+  &__description {
+    max-width: 670px;
+    margin: 10px auto 0;
+    color: var(--color-gray-dark);
+    font-size: var(--fs-m);
+    line-height: 25px;
+    text-align: center;
+  }
+}
+</style>
