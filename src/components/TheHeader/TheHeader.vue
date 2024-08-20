a949dd336fe041adba132a5c4a6acc7c0d60340c
Hardart <kaino@bk.ru>
2024-08-20 17:22:55 +0300

    feat: add new fonts and update color variables

---
+++ src/components/TheHeader/TheHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import RsHeaderTopNavbar from '@/components/HeaderTopNavbar/HeaderTopNavbar.vue'
+import RsHeaderMiddleNavbar from '@/components/HeaderMiddleNavbar/HeaderMiddleNavbar.vue'
+import RsHeaderBottomNavbar from '@/components/HeaderBottomNavbar/HeaderBottomNavbar.vue'
+</script>
+
+<template>
+  <header class="rs-header">
+    <RsHeaderTopNavbar />
+    <RsHeaderMiddleNavbar />
+    <RsHeaderBottomNavbar />
+  </header>
+</template>
+
+<style lang="scss">
+@import './TheHeader.scss';
+</style>
---
+++ src/contexts/homeViewContext/views/HomeView/components/SpecialOffer/SpecialOffer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="special-offer">
+    <img class="special-offer__image" src="@/assets/images/offer.png" />
+    <h4 class="special-offer__title">название акции</h4>
+    <p class="special-offer__description">
+      Меня не спрашивали, а надо было спросить, что означает имя Заратустры
+      именно в моих устах — в устах первого имморалиста: ведь то, в чём состоит
+      неслыханная уникальность этого перса в истории, являет собою
+      противоположность как раз этому.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles.scss';
+</style>
