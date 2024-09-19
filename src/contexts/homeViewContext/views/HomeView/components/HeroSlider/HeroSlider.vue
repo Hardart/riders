2dc145ec279db3d60970000dcd5031e73d9e582b
Hardart <kaino@bk.ru>
2024-09-19 19:09:45 +0300

    refactor: update Hero Slider structure, add container, navigation dots, and image classes

--- src/contexts/homeViewContext/views/HomeView/components/HeroSlider/HeroSlider.vue
+++ src/contexts/homeViewContext/views/HomeView/components/HeroSlider/HeroSlider.vue
@@ -4,17 +4,37 @@ import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
 
 <template>
   <div class="hero-slider">
-    <div class="hero-slider__items">
-      <div class="hero-slider__item">
-        <img src="@/assets/images/1.png" width="1360" height="482" alt="" />
-      </div>
-      <div class="hero-slider__item">
-        <p class="hero-slider__title">winter sale до -70%</p>
-        <img src="@/assets/images/2.png" width="1360" height="482" alt="" />
-      </div>
-      <div class="hero-slider__item">
-        <p class="hero-slider__title">LIB TECH</p>
-        <img src="@/assets/images/3.png" width="1360" height="482" alt="" />
+    <div class="hero-slider__container">
+      <div class="hero-slider__items">
+        <div class="hero-slider__item">
+          <img
+            src="@/assets/images/1.png"
+            class="hero-slider__image"
+            width="1360"
+            height="482"
+            alt=""
+          />
+        </div>
+        <div class="hero-slider__item">
+          <p class="hero-slider__title">winter sale до -70%</p>
+          <img
+            src="@/assets/images/2.png"
+            class="hero-slider__image"
+            width="1360"
+            height="482"
+            alt=""
+          />
+        </div>
+        <div class="hero-slider__item">
+          <p class="hero-slider__title">LIB TECH</p>
+          <img
+            src="@/assets/images/3.png"
+            class="hero-slider__image"
+            width="1360"
+            height="482"
+            alt=""
+          />
+        </div>
       </div>
     </div>
     <div class="slider-navigation slider-navigation--left">
@@ -23,6 +43,13 @@ import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
     <div class="slider-navigation slider-navigation--right">
       <SvgIcon icon="chevronLeft" class="slider-navigation__icon" />
     </div>
+    <ul class="slider-navigation__dots">
+      <li class="slider-navigation__dot slider-navigation__dot--active"></li>
+      <li class="slider-navigation__dot"></li>
+      <li class="slider-navigation__dot"></li>
+      <li class="slider-navigation__dot"></li>
+      <li class="slider-navigation__dot"></li>
+    </ul>
   </div>
 </template>
 
