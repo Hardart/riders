071b11912b94861e9885b39b3db5b3b6cd71d719
Hardart <kaino@bk.ru>
2024-10-13 16:38:46 +0300

    feat: add new product colors component

---
+++ src/contexts/productCardViewContext/views/ProductCardView/components/Product/components/ProductColors/ProductColors.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="product-colors">
+    <p class="product-colors__title">Цвет: Цветной</p>
+    <ul class="product-colors__list">
+      <li class="product-colors__item product-colors__item--active">
+        <img
+          src="@/assets/images/image 9-3.png"
+          class="product-colors__image"
+          alt=""
+        />
+      </li>
+      <li class="product-colors__item">
+        <img
+          src="@/assets/images/image 9-3.png"
+          class="product-colors__image"
+          alt=""
+        />
+      </li>
+      <li class="product-colors__item">
+        <img
+          src="@/assets/images/image 9-3.png"
+          class="product-colors__image"
+          alt=""
+        />
+      </li>
+      <li class="product-colors__item">
+        <img
+          src="@/assets/images/image 9-3.png"
+          class="product-colors__image"
+          alt=""
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
