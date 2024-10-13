7eae3c60c657019a5989cee36c32b48896505cf9
Hardart <kaino@bk.ru>
2024-10-13 16:41:53 +0300

    feat: add new product sizes component

---
+++ src/contexts/productCardViewContext/views/ProductCardView/components/Product/components/ProductSizes/ProductSizes.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="product-sizes">
+    <p class="product-sizes__title">Таблица размеров</p>
+    <div class="product-sizes__control">
+      <button class="button button--square button--square-active">32,5</button>
+      <button class="button button--square">155</button>
+      <button class="button button--square">32,5</button>
+      <button class="button button--square">32</button>
+      <button class="button button--square">32,5</button>
+      <button class="button button--square">32,5</button>
+      <button class="button button--square">32,5</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
