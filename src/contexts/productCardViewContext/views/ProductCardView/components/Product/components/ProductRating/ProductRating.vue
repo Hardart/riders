700b6126a209e2a3a298f465d14072d4ca1d4d5b
Hardart <kaino@bk.ru>
2024-10-13 16:36:33 +0300

    feat: add new product rating component

---
+++ src/contexts/productCardViewContext/views/ProductCardView/components/Product/components/ProductRating/ProductRating.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
+</script>
+
+<template>
+  <div class="product-rating">
+    <ul class="product-rating__stars">
+      <li class="product-rating__item">
+        <SvgIcon
+          icon="star"
+          class="product-rating__icon product-rating__icon--active"
+        />
+      </li>
+      <li class="product-rating__item">
+        <SvgIcon
+          icon="star"
+          class="product-rating__icon product-rating__icon--active"
+        />
+      </li>
+      <li class="product-rating__item">
+        <SvgIcon
+          icon="star"
+          class="product-rating__icon product-rating__icon--active"
+        />
+      </li>
+      <li class="product-rating__item">
+        <SvgIcon
+          icon="star"
+          class="product-rating__icon product-rating__icon--active"
+        />
+      </li>
+      <li class="product-rating__item">
+        <SvgIcon icon="star" class="product-rating__icon" />
+      </li>
+    </ul>
+    <p class="product-rating__reviews">24 отзыва</p>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
