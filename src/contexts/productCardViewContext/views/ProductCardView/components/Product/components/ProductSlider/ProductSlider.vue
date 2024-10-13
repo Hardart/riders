b494b3b73b6b98668674b2f3814357cf511d879a
Hardart <kaino@bk.ru>
2024-10-13 16:59:13 +0300

    feat: add new product slider component and add mixin for styling scrollbar

---
+++ src/contexts/productCardViewContext/views/ProductCardView/components/Product/components/ProductSlider/ProductSlider.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="product-slider">
+    <div class="product-slider__preview">
+      <ul class="product-slider__list">
+        <li class="product-slider__item product-slider__item--active">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+        <li class="product-slider__item">
+          <img src="@/assets/images/image 9-3.png" alt="" />
+        </li>
+      </ul>
+    </div>
+    <div class="product-slider__view">
+      <img
+        src="@/assets/images/product-1-orig.png"
+        class="product-slider__image"
+      />
+    </div>
+    <ul class="slider-dot-nav product-slider__navigation">
+      <li class="slider-dot-nav__item slider-dot-nav__item--active"></li>
+      <li class="slider-dot-nav__item"></li>
+      <li class="slider-dot-nav__item"></li>
+      <li class="slider-dot-nav__item"></li>
+      <li class="slider-dot-nav__item"></li>
+    </ul>
+    <button class="product-slider__button">Видео</button>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
