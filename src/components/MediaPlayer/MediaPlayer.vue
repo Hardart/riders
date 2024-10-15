922302471964fd804b47c32f9ea7f3360b0cf2fb
Hardart <kaino@bk.ru>
2024-10-15 19:35:33 +0300

    feat: add new media player component

---
+++ src/components/MediaPlayer/MediaPlayer.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="media-player">
+    <div class="media-controls">
+      <div class="media-controls__play"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./MediaPlayer.scss" />
